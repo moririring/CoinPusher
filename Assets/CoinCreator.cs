18c163c51aaa86c8da418501a1a4be9b1d7035f6
moririring <moriri.ring10@gmail.com>
2014-07-05 10:46:45 +0900

    セーブ忘れたので再度

--- Assets/CoinCreator.cs
+++ Assets/CoinCreator.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class CoinCreator : MonoBehaviour {
 	public GameObject prefab;
+	public int ofsSpeed;
 	// Use this for initialization
 	void Start () {
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			var ofs = new Vector3(Mathf.Sin(Time.time * 5), 0 , 0);
+		if (Input.GetButtonDown ("Fire1") && Score.scoreCount > 0) {
+			var ofs = new Vector3(Mathf.Sin(Time.time * ofsSpeed), 0 , 0);
 			Instantiate (prefab, transform.position + ofs, transform.rotation);
 			Score.scoreCount -= 1;
 		}	
