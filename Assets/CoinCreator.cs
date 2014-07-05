3d1be6f339046e283f7d76ec36c7a2c279e056dd
moririring <moriri.ring10@gmail.com>
2014-07-05 21:38:02 +0900

    サウンド追加

--- Assets/CoinCreator.cs
+++ Assets/CoinCreator.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class CoinCreator : MonoBehaviour {
-	public GameObject prefab;
-	public int ofsSpeed;
+    public GameObject prefab;
+    private AudioSource audioSource;
+    public AudioClip audioClip;
+    public int ofsSpeed;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+	    audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
 	}
 	// Update is called once per frame
 	void Update () {
@@ -13,6 +18,7 @@ public class CoinCreator : MonoBehaviour {
 			var ofs = new Vector3(Mathf.Sin(Time.time * ofsSpeed), 0 , 0);
 			Instantiate (prefab, transform.position + ofs, transform.rotation);
 			Score.scoreCount -= 1;
+            audioSource.Play();
 		}	
 	}
 }
